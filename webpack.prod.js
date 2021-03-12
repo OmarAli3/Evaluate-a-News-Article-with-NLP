912a4ec1be04813b7ea5c232caff44a2dd78ebf1
Omar Ali Hassan <36759605+OmarAli3@users.noreply.github.com>
2021-03-12 16:35:24 +0200

    Making a common webpack config
    Converting css files to scss and making common configuration for development and production modes

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,23 +1,8 @@
 const path = require('path')
+const { merge } = require('webpack-merge')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
 
-module.exports = {
-    entry: './src/client/index.js',
-    mode: 'production',
-    module: {
-        rules: [
-            {
-                test: '/\.js$/',
-                exclude: /node_modules/,
-                loader: "babel-loader"
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebPackPlugin({
-            template: "./src/client/views/index.html",
-            filename: "./index.html",
-        })
-    ]
-}
+module.exports = merge(common,{
+    mode: 'production'
+})
