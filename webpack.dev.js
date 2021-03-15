5559455db83af2c815531fab41e4792cf8db4d18
Omar Ali Hassan <36759605+OmarAli3@users.noreply.github.com>
2021-03-15 20:18:13 +0200

    Moving service workers from dev to prod mode

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,7 +1,6 @@
 const { merge } = require('webpack-merge') 
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const common = require('./webpack.common.js')
-const workboxPlugin = require("workbox-webpack-plugin")
 
 module.exports = merge(common,{
     mode: 'development',
@@ -16,7 +15,6 @@ module.exports = merge(common,{
             // Automatically remove all unused webpack assets on rebuild
             cleanStaleWebpackAssets: true,
             protectWebpackAssets: false
-        }),
-        new workboxPlugin.GenerateSW()
+        })
     ]
 })
